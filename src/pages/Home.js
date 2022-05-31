4762ae8dca1259beff9a8d7ebfbbbe27881e90be
13prashant <pacific1392@gmail.com>
2022-05-31 21:46:49 +0530

    initial setup

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import HomeCarousel from "../components/Home/HomeCarousel";
+import Ads from "../components/Home/Ads";
+
+const Home = () => {
+  return (
+    <>
+      <HomeCarousel />
+      <Ads />
+      <Ads />
+    </>
+  );
+};
+
+export default Home;
